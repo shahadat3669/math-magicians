cf304febb64249430086eebb4cd157a8553de0c1
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-10 22:58:25 +0600

    Add calculator class in component folder.

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Calculator extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div className="calculator">from calculator jsx file</div>;
+  }
+}
+export default Calculator;
