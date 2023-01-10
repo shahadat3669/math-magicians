4c5c8173cdd61c45a0c57ef253605f2ffde35fbd
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-10 23:04:32 +0600

    Add buttons in ButtonGrid class.

--- src/components/ButtonGrid.jsx
+++ src/components/ButtonGrid.jsx
@@ -7,7 +7,67 @@ class ButtonGrid extends Component {
   }
 
   render() {
-    return <div className="key-grid">From buttongrid jsx file.</div>;
+    return (
+      <div className="key-grid">
+        <button type="button" className="button">
+          AC
+        </button>
+        <button type="button" className="button">
+          +/-
+        </button>
+        <button type="button" className="button">
+          %
+        </button>
+        <button type="button" className="button button--orange">
+          /
+        </button>
+        <button type="button" className="button">
+          7
+        </button>
+        <button type="button" className="button">
+          8
+        </button>
+        <button type="button" className="button">
+          9
+        </button>
+        <button type="button" className="button button--orange">
+          x
+        </button>
+        <button type="button" className="button">
+          4
+        </button>
+        <button type="button" className="button">
+          5
+        </button>
+        <button type="button" className="button">
+          6
+        </button>
+        <button type="button" className="button button--orange">
+          -
+        </button>
+        <button type="button" className="button">
+          1
+        </button>
+        <button type="button" className="button">
+          2
+        </button>
+        <button type="button" className="button">
+          3
+        </button>
+        <button type="button" className="button button--orange">
+          +
+        </button>
+        <button type="button" className="button button--zero">
+          0
+        </button>
+        <button type="button" className="button">
+          .
+        </button>
+        <button type="button" className="button button--orange">
+          =
+        </button>
+      </div>
+    );
   }
 }
 export default ButtonGrid;
