987a6ea70a39f7d8d5aba720d331210488844247
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-11 00:14:23 +0600

    Convert app.js functional component to class based component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <>
-      <Calculator />
-    </>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <main>
+        <Calculator />
+      </main>
+    );
+  }
+}
 export default App;
