c2a81de6c96bdc2fd934d0722f7b6f81726eb774
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-11 01:51:05 +0600

    Update display class with propTypes and use total, operation, next props.

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,17 +1,29 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Display extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {};
   }
 
   render() {
+    const { total, next, operation } = this.props;
     return (
       <div className="display">
-        <div className="display__inner">0123</div>
+        <div className="display__inner">
+          {total}
+          {operation}
+          {next}
+        </div>
       </div>
     );
   }
 }
 export default Display;
+
+Display.propTypes = {
+  total: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
+};
