623b10e24ed3c0e5961af613bc68191257742c69
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-11 01:47:38 +0600

    Add calculate function in Calculator file.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,16 @@
 import { Component } from 'react';
 import Display from './Display';
 import ButtonGrid from './ButtonGrid';
+import calculate from './logic/calculate';
 
 class Calculator extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
   render() {
