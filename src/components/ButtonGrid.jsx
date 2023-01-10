b073dba0300af4784ae075dfd69d3cbefee036e4
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-10 23:04:06 +0600

    Add ButtonGrid.jsx file.

---
+++ src/components/ButtonGrid.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class ButtonGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="key-grid">From buttongrid jsx file.</div>;
+  }
+}
+export default ButtonGrid;
