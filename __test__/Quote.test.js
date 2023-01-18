bf57952ea0832679ca541235db187592da5c1acb
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-19 04:46:43 +0600

    Add test for Quote file.

---
+++ __test__/Quote.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Quote from '../src/components/Quote';
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<Quote />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
