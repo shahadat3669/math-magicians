21b1754e30f18b05ae1f5737cc070392a1fc1ef7
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-19 02:34:17 +0600

    Add quote.jsx file.

---
+++ src/components/Quote.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <section className="quote">
+    <blockquote>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. -William Paul Thurston
+    </blockquote>
+  </section>
+);
+
+export default Quote;
