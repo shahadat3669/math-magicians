e6e08e2ee2a3f89a42487f595a50b659cbf15191
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-19 02:35:09 +0600

    Add home.jsx file.

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Home = () => (
+  <section className="home">
+    <h1 className="home__title">Welcome to our page!</h1>
+    <div className="home__description">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas itaque
+        suscipit amet odio voluptate excepturi corrupti repellendus odit
+        molestiae ullam rem porro facilis blanditiis quae animi, harum rerum
+        totam reiciendis vero ad accusamus possimus. Quibusdam, voluptates!
+        Soluta, suscipit quam. Iure, ex rem eveniet quaerat corporis cumque
+        similique labore perspiciatis numquam.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas itaque
+        suscipit amet odio voluptate excepturi corrupti repellendus odit
+        molestiae ullam rem porro facilis blanditiis quae animi, harum rerum
+        totam reiciendis vero ad accusamus possimus. Quibusdam, voluptates!
+        Soluta, suscipit quam. Iure, ex rem eveniet quaerat corporis cumque
+        similique labore perspiciatis numquam.
+      </p>
+    </div>
+  </section>
+);
+
+export default Home;
