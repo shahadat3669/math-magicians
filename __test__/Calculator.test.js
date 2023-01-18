2401ddbacf9082fa86be74e8b660fc6dcc455e13
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-19 04:47:44 +0600

    Add test file for calculator component.

---
+++ __test__/Calculator.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Calculator from '../src/components/Calculator';
+
+it('renders correctly when there are no items', () => {
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
