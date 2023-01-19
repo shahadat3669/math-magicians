b3a0ce7569ce18e7a23c32471f17e2009c72885c
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-19 14:54:12 -0500

    Update Display test file.

--- __test__/Display.test.js
+++ __test__/Display.test.js
@@ -4,6 +4,8 @@ import renderer from 'react-test-renderer';
 import Display from '../src/components/Display';
 
 it('renders correctly when there are no items', () => {
-  const tree = renderer.create(<Display />).toJSON();
+  const tree = renderer
+    .create(<Display total="" next="" operation="" />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
