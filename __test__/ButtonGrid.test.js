9fe20dbb690b2be87fbe8b356a880df1fa6e1817
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-19 14:53:23 -0500

    Fix buttonGrid test file warnings.

--- __test__/ButtonGrid.test.js
+++ __test__/ButtonGrid.test.js
@@ -4,6 +4,6 @@ import renderer from 'react-test-renderer';
 import ButtonGrid from '../src/components/ButtonGrid';
 
 it('renders correctly when there are no items', () => {
-  const tree = renderer.create(<ButtonGrid />).toJSON();
+  const tree = renderer.create(<ButtonGrid clickHandler={() => {}} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
