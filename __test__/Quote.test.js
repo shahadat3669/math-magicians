5527ef4b381481501129a4e875def92e2360a55f
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-19 14:33:12 -0500

    Create test for Quote.js

--- __test__/Quote.test.js
+++ __test__/Quote.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 
 import Quote from '../src/components/Quote';
 
-it('matches snapshot', () => {
+it('renders correctly when there are no items', () => {
   const tree = renderer.create(<Quote />).toJSON();
   expect(tree).toMatchSnapshot();
 });
